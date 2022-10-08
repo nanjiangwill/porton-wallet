74fc1084c7e06cb8da1015ee2f7298e8a6d3a420
Will Jiang <willjiang2018@gmail.com>
2022-10-08 00:56:41 -0500

    update test file

--- contracts/test/MasterOfPaymaster.t.sol
+++ contracts/test/MasterOfPaymaster.t.sol
@@ -2,22 +2,6 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Counter.sol";
 
 contract MasterOfPaymasterTest is Test {
-    Counter public counter;
-    function setUp() public {
-       counter = new Counter();
-       counter.setNumber(0);
-    }
-
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }
 }
