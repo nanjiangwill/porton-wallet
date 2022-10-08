c75dd8e73b93e2bdd4656572a7ec78220e8dad77
Will Jiang <willjiang2018@gmail.com>
2022-10-08 00:47:06 -0500

    import required sol files

--- contracts/script/MasterOfPaymaster.s.sol
+++ contracts/script/MasterOfPaymaster.s.sol
@@ -3,11 +3,9 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 
-contract CounterScript is Script {
+contract MasterOfPaymasterScript is Script {
     function setUp() public {}
     function run() public {
         vm.broadcast();
-
-        adsf
     }
 }
--- contracts/test/MasterOfPaymaster.t.sol
+++ contracts/test/MasterOfPaymaster.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/Counter.sol";
 
-contract CounterTest is Test {
+contract MasterOfPaymasterTest is Test {
     Counter public counter;
     function setUp() public {
        counter = new Counter();
