c440fd5a4d86f186dbf26142a7bc3d4782762f61
onjas <onjas_buidl@proton.me>
2022-10-09 05:45:02 -0500

    fix USDC balance display

--- packages/frontend/src/pages/Profile.tsx
+++ packages/frontend/src/pages/Profile.tsx
@@ -13,10 +13,266 @@ const ProfilePage = () => {
   const [ethBalance, setETHBalance] = useState(0)
 
   useEffect(() => {
-    provider
+    const loadData = async () => {
+      provider
       .getBalance(address!)
       .then(balance => setETHBalance(balance.toNumber()))
-  }, [address])
+
+      let num2 = await getUsdcBalance(address!)
+      setUSDCBalance(num2)
+
+    }
+
+    }, [address])
+
+  const [usdcBalance, setUSDCBalance] = useState(0)
+  // const usdcContractAddr = "0x07865c6e87b9f70255377e024ace6630c1eaa37f"
+
+  // const provider = ...; (use ethers.providers.InfuraProvider for a Node app or ethers.providers.Web3Provider(window.ethereum/window.web3) for a React app)
+
+  // const balance = (contract.balanceOf(address!)).toNumber();
+  async function getUsdcBalance(addr: string) {
+    const ethers = require('ethers');
+    const genericErc20Abi = [
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+          {
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_spender",
+            "type": "address"
+          },
+          {
+            "name": "_value",
+            "type": "uint256"
+          }
+        ],
+        "name": "approve",
+        "outputs": [
+          {
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_from",
+            "type": "address"
+          },
+          {
+            "name": "_to",
+            "type": "address"
+          },
+          {
+            "name": "_value",
+            "type": "uint256"
+          }
+        ],
+        "name": "transferFrom",
+        "outputs": [
+          {
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint8"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [
+          {
+            "name": "_owner",
+            "type": "address"
+          }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+          {
+            "name": "balance",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+          {
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "constant": false,
+        "inputs": [
+          {
+            "name": "_to",
+            "type": "address"
+          },
+          {
+            "name": "_value",
+            "type": "uint256"
+          }
+        ],
+        "name": "transfer",
+        "outputs": [
+          {
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "constant": true,
+        "inputs": [
+          {
+            "name": "_owner",
+            "type": "address"
+          },
+          {
+            "name": "_spender",
+            "type": "address"
+          }
+        ],
+        "name": "allowance",
+        "outputs": [
+          {
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "payable": true,
+        "stateMutability": "payable",
+        "type": "fallback"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "name": "owner",
+            "type": "address"
+          },
+          {
+            "indexed": true,
+            "name": "spender",
+            "type": "address"
+          },
+          {
+            "indexed": false,
+            "name": "value",
+            "type": "uint256"
+          }
+        ],
+        "name": "Approval",
+        "type": "event"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "name": "from",
+            "type": "address"
+          },
+          {
+            "indexed": true,
+            "name": "to",
+            "type": "address"
+          },
+          {
+            "indexed": false,
+            "name": "value",
+            "type": "uint256"
+          }
+        ],
+        "name": "Transfer",
+        "type": "event"
+      }
+    ];
+    const tokenContractAddress = '0x07865c6e87b9f70255377e024ace6630c1eaa37f';
+    const contract = new ethers.Contract(tokenContractAddress, genericErc20Abi, provider);
+    let num = await contract.balanceOf(addr)
+    console.log(num)
+    console.log(num.toNumber())
+    let a = num.toNumber()
+    return a
+  }
+
+
+
+
+  //   async function getAmount(currency, address) {
+  //     const amount = await currency.contract.methods.balanceOf(address).call();
+  //     return amount * currency.usdrate;
+  //  }
+
+
 
   return (
     <Container className={styles.page} maxW="container.lg" py={16}>
@@ -51,7 +307,7 @@ const ProfilePage = () => {
         </Box>
         <Box px={5} py={2} bgColor="#fafafa" borderRadius="2xl">
           <Text fontSize="5xl" fontWeight="light">
-            0 USDC
+            {usdcBalance} USDC
           </Text>
         </Box>
       </Box>
